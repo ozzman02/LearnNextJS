e5ca07466f6e594c26084196c6539fec003afc92
Oscar Santamaria Venegas <osantamaria@gmail.com>
2025-04-18 09:20:11 -0600

    nextjs-essentials II

---
+++ first-nextjs-app/components/header.js
@@ -0,0 +1,10 @@
+import { Fragment } from "react";
+
+export default function Header() {
+	return (
+		<Fragment>
+			<img src="/logo.png" alt="A server surrounded by magic sparkles." />
+			<h1>Welcome to this NextJS Course!</h1>
+		</Fragment>
+	);
+}
