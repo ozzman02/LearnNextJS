d0097939bff662a3e9f80ba5935e10a76e56c31c
Oscar Santamaria Venegas <osantamaria@gmail.com>
2025-05-22 19:12:34 -0600

    news-app interception routes

--- news-app/app/news/[slug]/page.js
+++ news-app/app/news/[slug]/page.js
@@ -1,4 +1,5 @@
 import { DUMMY_NEWS } from "@/dummy-news";
+import Link from "next/link";
 import { notFound } from "next/navigation";
 
 export default function NewsDetailPage({ params }) {
@@ -18,7 +19,10 @@ export default function NewsDetailPage({ params }) {
 	return (
 		<article className="news-article">
 			<header>
-				<img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
+				{/* /news/${newsItem.slug}/image is the path of the page that is news/[slug]/image/page */}
+				<Link href={`/news/${newsItem.slug}/image`}>
+					<img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
+				</Link>
 				<h1>{newsItem.title}</h1>
 				<time dataTime={newsItem.date}>{newsItem.date}</time>
 			</header>
