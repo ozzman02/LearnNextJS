4b67033e9d7c783c5ac0d82b04c58d3e763b3ff7
Oscar Santamaria Venegas <osantamaria@gmail.com>
2025-05-24 21:00:42 -0600

    news-app data fetching strategies

--- news-app/app/(content)/news/page.js
+++ news-app/app/(content)/news/page.js
@@ -1,51 +1,16 @@
-'use client';
-
 import NewsList from "@/components/news-list";
-//import { DUMMY_NEWS } from "@/dummy-news";
-import { Fragment, useEffect, useState } from "react";
+import { getAllNews } from "@/lib/news";
+import { Fragment } from "react";
 
 
-export default function NewsPage() {
+export default async function NewsPage() {
 
-	const [error, setError] = useState();
-
-	const [isLoading, setIsLoading] = useState(false);
-
-	const [news, setNews] = useState();
-
-	useEffect(() => {
-		async function fetchNews() {
-			setIsLoading(true);
-			const response = await fetch('http://localhost:8080/news');
-			if (!response.ok) {
-				setError('Failed to fetch news.');
-				setIsLoading(false);
-			}
-			const news = await response.json();
-			setIsLoading(false);
-			setNews(news)
-		}
-		fetchNews();
-	}, []);
-
-	if (isLoading) {
-		return <p>Loading...</p>
-	}
-
-	if (error) {
-		return <p>{error}</p>
-	}
-
-	let newsContent;
-
-	if (news) {
-		newsContent = <NewsList news={news} />
-	}
+	const news = getAllNews();
 
 	return (
 		<Fragment>
 			<h1>News Page</h1>
-			{newsContent}
+			<NewsList news={news} />
 		</Fragment>
 	);
 }
