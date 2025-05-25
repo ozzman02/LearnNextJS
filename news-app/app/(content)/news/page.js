47435971c6584584f9650a0c284f5cce9fcbfbda
Oscar Santamaria Venegas <osantamaria@gmail.com>
2025-05-24 19:23:39 -0600

    news-app adding the backend and client side data fetching

--- news-app/app/(content)/news/page.js
+++ news-app/app/(content)/news/page.js
@@ -1,13 +1,51 @@
+'use client';
+
 import NewsList from "@/components/news-list";
-import { DUMMY_NEWS } from "@/dummy-news";
-import { Fragment } from "react";
+//import { DUMMY_NEWS } from "@/dummy-news";
+import { Fragment, useEffect, useState } from "react";
 
 
 export default function NewsPage() {
+
+	const [error, setError] = useState();
+
+	const [isLoading, setIsLoading] = useState(false);
+
+	const [news, setNews] = useState();
+
+	useEffect(() => {
+		async function fetchNews() {
+			setIsLoading(true);
+			const response = await fetch('http://localhost:8080/news');
+			if (!response.ok) {
+				setError('Failed to fetch news.');
+				setIsLoading(false);
+			}
+			const news = await response.json();
+			setIsLoading(false);
+			setNews(news)
+		}
+		fetchNews();
+	}, []);
+
+	if (isLoading) {
+		return <p>Loading...</p>
+	}
+
+	if (error) {
+		return <p>{error}</p>
+	}
+
+	let newsContent;
+
+	if (news) {
+		newsContent = <NewsList news={news} />
+	}
+
 	return (
 		<Fragment>
 			<h1>News Page</h1>
-			<NewsList news={DUMMY_NEWS} />
+			{newsContent}
 		</Fragment>
 	);
 }
