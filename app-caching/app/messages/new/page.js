bbc766e271d2e0da394d79e338a004d2abf2b090
Oscar Santamaria Venegas <osantamaria@gmail.com>
2025-06-08 20:40:04 -0600

    app-cache review

---
+++ app-caching/app/messages/new/page.js
@@ -0,0 +1,36 @@
+import { redirect } from 'next/navigation';
+
+import { addMessage } from '@/lib/messages';
+import { revalidatePath } from 'next/cache';
+
+export default function NewMessagePage() {
+	async function createMessage(formData) {
+		'use server';
+
+		const message = formData.get('message');
+		addMessage(message);
+
+		/*
+			All the data and route cache related to this path will be deleted.
+			Any nested paths and nested pages will not have their data deleted or revalidated unless you specify a second argument.
+		*/
+		revalidatePath('/messages', 'layout');
+		redirect('/messages');
+	}
+
+	return (
+		<>
+			<h2>New Message</h2>
+			<form action={createMessage}>
+				<p className="form-control">
+					<label htmlFor="message">Your Message</label>
+					<textarea id="message" name="message" required rows="5" />
+				</p>
+
+				<p className="form-actions">
+					<button type="submit">Send</button>
+				</p>
+			</form>
+		</>
+	);
+}
